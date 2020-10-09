10882a6e758bf8bc63c22d1989d0cd4c98592272
Daniel <daniel@magnitude.io>
2020-10-08 21:24:46 -0700

    picks a random strategy on start

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ var board = new five.Board({
   io: new tessel()
 })
 
+function pickRandom() {
+  var length = strategies.length
+  var random = Math.floor( Math.random() * length)
+  return random
+}
+
 board.on('ready', function() {
   var lcd = new five.LCD({
     pins: ['b2', 'b3', 'b4', 'b5', 'b6', 'b7']
   })
 
+  var randomIndex = pickRandom()
+
   lcd.cursor(0, 0).print(
-    strategies[0]
+    strategies[ randomIndex ]
   )
 
 })
